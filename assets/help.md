b02ceab3721f70fde00ea60f0e9d2354dbd84740
Edouard Cunibil <edouard@happyculture.coop>
2021-04-13 18:39:36 +0200

    Replace help command output by a md file.

---
+++ assets/help.md
@@ -0,0 +1,4 @@
+**Obtenir l\'almanach du jour** :
+Permet de connaître le jour de la semaine, son élément et les conjonctions potentielles.
+Syntaxe : @JJ/MM/YYYY
+Exemples : @01/01/2017 ou @30/06/-152
--- includes/commands/help.js
+++ includes/commands/help.js
@@ -1,12 +1,11 @@
+const { readFile } = require('fs').promises;
 
 function handle(msg) {
   if (msg.content === '@help') {
-    let response = [];
-    response.push('**Obtenir l\'almanach du jour** :');
-    response.push('Permet de connaître le jour de la semaine, son élément et les conjonctions potentielles.');
-    response.push('Syntaxe : @JJ/MM/YYYY');
-    response.push('Exemples : @01/01/2017 ou @30/06/-152');
-    msg.channel.send(response.join("\n"));
+    readFile('./assets/help.md', {encoding: 'utf8'})
+      .then((data) => {
+        msg.channel.send(data);
+      });
   }
 }
 
